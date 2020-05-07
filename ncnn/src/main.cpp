5b6282c5c33edc98999084a9d6e120aa64544a24
mengfu188 <1242733702@qq.com>
2020-05-07 10:04:21 +0800

    fix ncnn demo

--- ncnn/src/main.cpp
+++ ncnn/src/main.cpp
@@ -9,37 +9,77 @@
 #include "UltraFace.hpp"
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/video/video.hpp>
 
 int main(int argc, char **argv) {
-    if (argc <= 3) {
-        fprintf(stderr, "Usage: %s <ncnn bin> <ncnn param> [image files...]\n", argv[0]);
+    if (argc <= 4) {
+        fprintf(stderr, "Usage: %s <ncnn bin> <ncnn param> <type:video|image> [files...]\n", argv[0]);
         return 1;
     }
 
     std::string bin_path = argv[1];
     std::string param_path = argv[2];
-    UltraFace ultraface(bin_path, param_path, 320, 240, 1, 0.7); // config model input
+    UltraFace ultraface(bin_path, param_path, 96, 128, 1, 0.7); // config model input
 
-    for (int i = 3; i < argc; i++) {
-        std::string image_file = argv[i];
-        std::cout << "Processing " << image_file << std::endl;
+    std::string type = argv[3];
+    if (type == "image") {
+        printf("image\n");
 
-        cv::Mat frame = cv::imread(image_file);
-        ncnn::Mat inmat = ncnn::Mat::from_pixels(frame.data, ncnn::Mat::PIXEL_BGR2RGB, frame.cols, frame.rows);
 
-        std::vector<FaceInfo> face_info;
-        ultraface.detect(inmat, face_info);
+        for (int i = 4; i < argc; i++) {
+            std::string image_file = argv[i];
+            std::cout << "Processing " << image_file << std::endl;
 
-        for (int i = 0; i < face_info.size(); i++) {
-            auto face = face_info[i];
-            cv::Point pt1(face.x1, face.y1);
-            cv::Point pt2(face.x2, face.y2);
-            cv::rectangle(frame, pt1, pt2, cv::Scalar(0, 255, 0), 2);
+            cv::Mat frame = cv::imread(image_file);
+            ncnn::Mat inmat = ncnn::Mat::from_pixels(frame.data, ncnn::Mat::PIXEL_BGR2RGB, frame.cols, frame.rows);
+
+            std::vector<FaceInfo> face_info;
+            ultraface.detect(inmat, face_info);
+
+            for (int i = 0; i < face_info.size(); i++) {
+                auto face = face_info[i];
+                cv::Point pt1(face.x1, face.y1);
+                cv::Point pt2(face.x2, face.y2);
+                cv::rectangle(frame, pt1, pt2, cv::Scalar(0, 255, 0), 2);
+            }
+
+            cv::imshow("UltraFace", frame);
+            cv::waitKey();
+//            cv::imwrite("result.jpg", frame);
         }
+    } else if (type == "video") {
+        printf("video\n");
+        for (int i = 4; i < argc; i++) {
+            cv::VideoCapture cap(argv[i]);
 
-        cv::imshow("UltraFace", frame);
-        cv::waitKey();
-        cv::imwrite("result.jpg", frame);
+            while (true) {
+                cv::Mat frame;
+                cap.read(frame);
+                if (frame.empty()) {
+
+                    cap.release();
+                    break;
+                }
+                ncnn::Mat inmat = ncnn::Mat::from_pixels(frame.data, ncnn::Mat::PIXEL_BGR2RGB, frame.cols, frame.rows);
+
+                std::vector<FaceInfo> face_info;
+                ultraface.detect(inmat, face_info);
+
+                for (int i = 0; i < face_info.size(); i++) {
+                    auto face = face_info[i];
+                    cv::Point pt1(face.x1, face.y1);
+                    cv::Point pt2(face.x2, face.y2);
+                    cv::rectangle(frame, pt1, pt2, cv::Scalar(0, 255, 0), 2);
+                }
+
+                cv::imshow("UltraFace", frame);
+                cv::waitKey(1);
+//                cv::imwrite("result.jpg", frame);
+            }
+
+        }
+    } else {
+        printf("type error!!!\n");
     }
     return 0;
 }
